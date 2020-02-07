037e57c92f11ef18f39e39b9f715cc50bfa1bde6
daniesamcrack <danielsam.fergu@gmail.com.com>
2020-02-07 14:49:05 -0400

    Initial commit

---
+++ app/src/main/java/voidlemonds/com/MainActivity.java
@@ -0,0 +1,15 @@
+package voidlemonds.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // wena 1.1
+    }
+}
